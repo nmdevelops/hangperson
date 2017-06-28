93edf0d82413df7906cff8d5ca5441de33683153
Nathan Mayer <nm@gmail.com>
2017-06-28 15:43:08 -0700

    add random word selection from worlBank of commomn core vocab

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,37 @@
-//working section
+//Business Logic (Back-end)
 var guess;// = prompt("What's your guess?");
 var validate;
-var targetWord = "donkey";
+var randomIndex;
+var targetWord;
 var wordBlanks = [];
 var wordBlanksString;
-var  wordBlanksStringClean;
+var wordBlanksStringClean;
 var wrongGuess = [];
 var notIt = [];
-var iteration = targetWord.length;
+var iteration;
+
 var alphabetPublic = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var wordBank = ["Ache","adjust","affordable","alarm","alone","apologize","appetite","applause","artistic","atmosphere","attach","Bashful","basket","batch","behave","belong","bend","blink","blush","bolt","bolts","borrow","bundle","Cabin","caterpillar","caution","cave","celebrate","centaur","champion","chat","cheat","chimney","compass","complain","conductor","construct","costume","cozy","cranky","crash","creak","croak","crowded","cue","curved","Daily","dainty","dart","decorate","delighted","denied","deserve","divide","dodge","drenched","drowsy","Enormous","equal","exclaim","exhausted","expensive","Fancy","fasten","filthy","flat","flee","fog","footprint","forest","freezing","Gather","giant","glad","gleaming","glum","grab","grateful","grin","grip","groan","Hatch","heap","hide","hobby","honest","howl","Illustrator","injury","Jealous","Knob","Lively","loosen","Mask","misty","modern","mountain","Narrow","Obey","Pain","passenger","pattern","pest","polish","pretend","promise","Rapid","remove","repeat","rescue","restart","return","ripe","rise","roar","rough","rusty","Scold","scratch","seed","selfish","serious","shell","shovel (verb)","shriek","sibling","silent","simple","slippery","sly","sneaky","sob","spiral","splendid","sprinkle","squirm","startle","steep","stormy","striped","surround","switch","Terrified","thick","thunder","ticket","timid","transportation","travel","trust","Upset","Weed","whimper","whirl","wicked","wicked","Yank"
+];
 
+var randomize = (function(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  randomIndex = Math.floor(Math.random() * (max - min + 1)) + min;
+});
 
+var wordGenerator = (function() {
+  randomize(0,wordBank.length);
+  targetWord = wordBank [randomIndex];
+  iteration = targetWord.length;
+});
 
-//Business Logic (Back-end)
 var gameSetup = (function() {
+  wordBlanks = [];
   for (var n = 0; n < iteration; n++) {
     wordBlanks.push("_");
   }
-  iteration -= targetWord.length;
+    iteration -= targetWord.length;
 
   $(".userInputs").show();
   $(".beginGame").hide();
@@ -28,7 +42,6 @@ var gameSetup = (function() {
   $("#wordBlanks").text(wordBlanksStringClean);
   wrongGuess = [];
   $("#wrongGuess").empty();
-
 });
 
 
@@ -74,7 +87,7 @@ var hangperson = (function() {
       notIt.push(guess);
     }
   }
-  if (notIt.length === 6) {
+  if (notIt.length === targetWord.length) {
     wrongGuess.push(guess);
     $("#wrongGuess").text("Wrong guess: " + wrongGuess);
   }
@@ -91,6 +104,8 @@ var hangperson = (function() {
 //User Logic (Front-end)
 $(document).ready(function() {
   $("button#gameSetup.btn").click(function() {
+    // location.reload();
+    wordGenerator();
     gameSetup();
   });
 
