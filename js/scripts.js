14113a390202d134c78ff6c31fc449cb6824b0dc
Nathan Mayer <nm@gmail.com>
2017-06-28 15:49:00 -0700

    fix uppercase bug in wordBank

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,7 @@ var notIt = [];
 var iteration;
 
 var alphabetPublic = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var wordBank = ["Ache","adjust","affordable","alarm","alone","apologize","appetite","applause","artistic","atmosphere","attach","Bashful","basket","batch","behave","belong","bend","blink","blush","bolt","bolts","borrow","bundle","Cabin","caterpillar","caution","cave","celebrate","centaur","champion","chat","cheat","chimney","compass","complain","conductor","construct","costume","cozy","cranky","crash","creak","croak","crowded","cue","curved","Daily","dainty","dart","decorate","delighted","denied","deserve","divide","dodge","drenched","drowsy","Enormous","equal","exclaim","exhausted","expensive","Fancy","fasten","filthy","flat","flee","fog","footprint","forest","freezing","Gather","giant","glad","gleaming","glum","grab","grateful","grin","grip","groan","Hatch","heap","hide","hobby","honest","howl","Illustrator","injury","Jealous","Knob","Lively","loosen","Mask","misty","modern","mountain","Narrow","Obey","Pain","passenger","pattern","pest","polish","pretend","promise","Rapid","remove","repeat","rescue","restart","return","ripe","rise","roar","rough","rusty","Scold","scratch","seed","selfish","serious","shell","shovel (verb)","shriek","sibling","silent","simple","slippery","sly","sneaky","sob","spiral","splendid","sprinkle","squirm","startle","steep","stormy","striped","surround","switch","Terrified","thick","thunder","ticket","timid","transportation","travel","trust","Upset","Weed","whimper","whirl","wicked","wicked","Yank"
-];
+var wordBank = ["ache","adjust","affordable","alarm","alone","apologize","appetite","applause","artistic","atmosphere","attach","bashful","basket","batch","behave","belong","bend","blink","blush","bolt","bolts","borrow","bundle","cabin","caterpillar","caution","cave","celebrate","centaur","champion","chat","cheat","chimney","compass","complain","conductor","construct","costume","cozy","cranky","crash","creak","croak","crowded","cue","curved","daily","dainty","dart","decorate","delighted","denied","deserve","divide","dodge","drenched","drowsy","enormous","equal","exclaim","exhausted","expensive","fancy","fasten","filthy","flat","flee","fog","footprint","forest","freezing","gather","giant","glad","gleaming","glum","grab","grateful","grin","grip","groan","hatch","heap","hide","hobby","honest","howl","illustrator","injury","jealous","knob","lively","loosen","mask","misty","modern","mountain","narrow","obey","pain","passenger","pattern","pest","polish","pretend","promise","rapid","remove","repeat","rescue","restart","return","ripe","rise","roar","rough","rusty","scold","scratch","seed","selfish","serious","shell","shovel (verb)","shriek","sibling","silent","simple","slippery","sly","sneaky","sob","spiral","splendid","sprinkle","squirm","startle","steep","stormy","striped","surround","switch","terrified","thick","thunder","ticket","timid","transportation","travel","trust","upset","weed","whimper","whirl","wicked","wicked","yank"];
 
 var randomize = (function(min, max) {
   min = Math.ceil(min);
@@ -104,7 +103,6 @@ var hangperson = (function() {
 //User Logic (Front-end)
 $(document).ready(function() {
   $("button#gameSetup.btn").click(function() {
-    // location.reload();
     wordGenerator();
     gameSetup();
   });
