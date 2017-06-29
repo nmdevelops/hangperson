6a7c55ce0f045ad707d4c4d753878e7b0a4e0d03
Nathan Mayer <nm@gmail.com>
2017-06-29 09:23:20 -0700

    add 80s typography by David Parker to style web app

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,14 @@ var iteration;
 var alphabetPublic = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var wordBank = ["ache","adjust","affordable","alarm","alone","apologize","appetite","applause","artistic","atmosphere","attach","bashful","basket","batch","behave","belong","bend","blink","blush","bolt","bolts","borrow","bundle","cabin","caterpillar","caution","cave","celebrate","centaur","champion","chat","cheat","chimney","compass","complain","conductor","construct","costume","cozy","cranky","crash","creak","croak","crowded","cue","curved","daily","dainty","dart","decorate","delighted","denied","deserve","divide","dodge","drenched","drowsy","enormous","equal","exclaim","exhausted","expensive","fancy","fasten","filthy","flat","flee","fog","footprint","forest","freezing","gather","giant","glad","gleaming","glum","grab","grateful","grin","grip","groan","hatch","heap","hide","hobby","honest","howl","illustrator","injury","jealous","knob","lively","loosen","mask","misty","modern","mountain","narrow","obey","pain","passenger","pattern","pest","polish","pretend","promise","rapid","remove","repeat","rescue","restart","return","ripe","rise","roar","rough","rusty","scold","scratch","seed","selfish","serious","shell","shovel (verb)","shriek","sibling","silent","simple","slippery","sly","sneaky","sob","spiral","splendid","sprinkle","squirm","startle","steep","stormy","striped","surround","switch","terrified","thick","thunder","ticket","timid","transportation","travel","trust","upset","weed","whimper","whirl","wicked","wicked","yank"];
 
+/*80's Typography by David Parker*/
+// if ( navigator.platform.indexOf('Win') != -1 ) {
+//   window.document.getElementById("wrapper").setAttribute("class", "windows");
+// } else if ( navigator.platform.indexOf('Mac') != -1 ) {
+//   window.document.getElementById("wrapper").setAttribute("class", "mac");
+// }
+/*80's Typography by David Parker*/
+
 var randomize = (function(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
